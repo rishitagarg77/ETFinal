93b1a287d55608d13e7a30c38e2cc26f0200b128
Rishav3102 <rishavbhalru31@gmail.com>
2023-06-06 21:09:46 +0530

    first commit

---
+++ Blog-Website/client/src/components/about/About.jsx
@@ -0,0 +1,47 @@
+
+import { Box, styled, Typography, Link } from '@mui/material';
+import { GitHub, Instagram, Email } from '@mui/icons-material';
+
+const Banner = styled(Box)`
+    background-image: url(https://www.wallpapertip.com/wmimgs/23-236943_us-wallpaper-for-website.jpg);
+    width: 100%;
+    height: 50vh;
+    background-position: left 0px bottom 0px;
+    background-size: cover;
+`;
+
+const Wrapper = styled(Box)`
+    padding: 20px;
+    & > h3, & > h5 {
+        margin-top: 50px;
+    }
+`;
+
+const Text = styled(Typography)`
+    color: #878787;
+`;
+
+const About = () => {
+
+    return (
+        <Box>
+            <Banner/>
+            <Wrapper>
+                <Typography variant="h3">BLOG</Typography>
+                <Text variant="h5">I am an undergraduate computer science student pursuing CSE from Chitkara University.
+                        I have worked on various web projects.<br />
+                    
+                    <Box component="span" style={{ marginLeft: 5 }}>
+                    </Box>
+                </Text>
+                <Text variant="h5">
+                    
+                    <Box component="span" style={{ marginLeft: 5 }}>
+                    </Box>  
+                </Text>
+            </Wrapper>
+        </Box>
+    )
+}
+
+export default About;
